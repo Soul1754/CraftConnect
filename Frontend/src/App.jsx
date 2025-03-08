8c244efefabe797ce044aede934ba7efd84353f5
sarthakgadekar-03 <sarthak.gadekar221@vit.edu>
2025-03-08 22:21:45 +0530

    Minor Bug Fixed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./components/pages/LandingPage";
+import LandingPage from "./Components/pages/LandingPage";
 import Login from "./Components/Pages/Onboarding/Login";
 import Signup from "./Components/Pages/Onboarding/Signup";
 import CustomerSignup from "./Components/Pages/Onboarding/CustomerSignup";
